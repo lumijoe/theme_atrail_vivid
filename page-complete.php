fb0c4b552aecec757a0e8862101ec174829185ef
小城 <kojolumi@gamil.com>
2024-09-10 14:05:20 +0900

    test

--- page-complete.php
+++ page-complete.php
@@ -9,8 +9,8 @@ get_header('contact');
         <div class="l-company-inner l-section-inner-plf--20px l-section-inner">
             <h1 class="p-section_ttl--pb32">送信が完了しました</h1>
             <p>お問い合わせありがとうございました。
-                <br>自動返信メールをお送りしましたのでご確認をお願いいたします。
-                <br>いただいた内容を確認次第、担当者よりご連絡させていただきます。
+                <br>自動返信メールをお送りしましたので<br class="pc-none">ご確認をお願いいたします。
+                <br><br class="pc-none">いただいた内容を確認次第<br class="pc-none">担当者よりご連絡させていただきます。
             </p>
             <br><br>
             <!-- to top -->
