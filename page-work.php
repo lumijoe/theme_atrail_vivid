13184a75dbcfdd8cc9f08aa7f5e6755a138d6758
lumijoe <spacedesign63@gmail.com>
2024-09-15 23:40:44 +0900

    test

--- page-work.php
+++ page-work.php
@@ -30,7 +30,7 @@ get_header('contribution');
                                 while ($the_query->have_posts()) {
                                     $the_query->the_post(); //ループのカウントアップ
                                 ?>
-                                    <li class="works-list">
+                                    <li class="works-list" style="list-style: none!important;">
                                         <!-- パーマリンク -->
                                         <a class="works-link" href="<?php the_permalink(); ?>">
                                             <!-- 画像：カスタムフィールドwork_imgから画像のURLを取得 -->
@@ -194,134 +194,124 @@ get_header('contribution');
                                                 if ($work_img) :
                                                 ?>
                                                     <!-- 画像があればecho表示 -->
-                                                    <img src="<?php echo esc_url($work_img); ?>" alt="">
+                                                    <img src="<?php echo esc_url($work_img); ?>" alt="" style="max-width: 300px; max-height: 220px; aspect-ratio: 300 / 220;">
                                                 <?php else : ?>
                                                     <!-- 画像がなければデフォルトサムネを表示 -->
                                                     <img src="<?php the_post_thumbnail_url(); ?>" alt="">
                                                 <?php endif; ?>
                                             </div>
+                                            <!-- タイトル：施工名 -->
+                                            <h3 class="works-link-text" style="font-size: 16px; display: flex;">
+                                                <?php
+                                                $work_name = get_field('work_name'); // カスタムフィールド 'work_name' 
+                                                if ($work_name) : // あるか確認
+                                                ?>
+                                                    <?php echo esc_html($work_name); ?><!-- テキスト表示 -->
+                                                <?php else : ?>
+                                                    <!-- 文字列がない場合はデフォルトを表示 -->
+                                                <?php endif; ?>
+                                            </h3>
 
                                             <div class="works-link-body">
                                                 <!-- タクソノミーを取得する -->
                                                 <!-- タクソノミーが選択されていたら実行する -->
                                                 <!-- 複数のタクソノミーを配列で定義 -->
-                                                <?php
-                                                $taxonomies = ['worktype', 'styletype', 'sizetype'];
-                                                // 各タクソノミーごとのタームをループ処理
-                                                foreach ($taxonomies as $taxonomy) {
-                                                    // タクソノミーのタームを取得
-                                                    $terms = get_the_terms($post->ID, $taxonomy);
-                                                    if ($terms) {
-                                                ?>
-                                                        <ul class="works-link-category" style="display: flex; flex-direction: row; ">
-                                                            <!-- 取得できたタームの数だけループさせる -->
-                                                            <?php
-                                                            foreach ($terms as $term) {
-                                                            ?>
-                                                                <!-- タームの名前を出力  -->
-                                                                <li style="padding: 5px 10px; border-radius: 10px; border: 1px solid #232323; width: max-content;">＃<?php echo esc_html($term->name); ?>
-                                                                </li>
-                                                            <?php
-                                                            }
-                                                            ?>
-                                                        </ul>
-                                                <?php
+                                                <div style="display: flex; flex-wrap: wrap; gap: 5px; max-width: 300px;">
+                                                    <?php
+                                                    $taxonomies = ['worktype', 'styletype', 'sizetype'];
+                                                    // 各タクソノミーごとのタームをループ処理
+                                                    foreach ($taxonomies as $taxonomy) {
+                                                        // タクソノミーのタームを取得
+                                                        $terms = get_the_terms($post->ID, $taxonomy);
+                                                        if ($terms) {
+                                                    ?>
+                                                            <ul class="works-link-category" style="display: flex; flex-direction: row; gap:5px;">
+                                                                <!-- 取得できたタームの数だけループさせる -->
+                                                                <?php
+                                                                foreach ($terms as $term) {
+                                                                ?>
+                                                                    <!-- タームの名前を出力  -->
+                                                                    <li style="padding: 2px 8px; border-radius: 10px; border: 1px solid #232323; width: max-content; font-size:12px;">＃<?php echo esc_html($term->name); ?>
+                                                                    </li>
+                                                                <?php
+                                                                }
+                                                                ?>
+                                                            </ul>
+                                                    <?php
+                                                        }
                                                     }
-                                                }
+                                                    ?>
+                                                </div> <!--ここまでdiv ulループ-->
+                                            </div> <!--ここまでタクソノミーループdiv-->
+
+
+
+
+
+
+                                            <!-- カテゴリ -->
+                                            <!-- <p class="works-link-text">
+                                                <?php
+                                                $work_category = get_field('work_category');
+                                                if ($work_category) :
                                                 ?>
-                                                </p>
+                                                    <?php echo esc_html($work_category); ?>
+                                                <?php else : ?>
+                                        
+                                                <?php endif; ?>
+                                            </p> -->
 
+                                            <!-- <p class="works-link-text">アピールポイント |
+                                                <?php
+                                                $work_strong = get_field('work_strong');
+                                                if ($work_strong) :
+                                                ?>
+                                                    <?php echo esc_html($work_strong); ?>
+                                                <?php else : ?>
+                                                <?php endif; ?>
+                                            </p> -->
+                                            <!-- <p class="works-link-text" style="padding: 5px 10px; border-radius: 10px; border: 1px solid #232323; width: max-content;">#
+                                                <?php
+                                                $work_size = get_field('work_size');
+                                                if ($work_size) :
+                                                ?>
+                                                    <?php echo esc_html($work_size); ?>
+                                                <?php else : ?>
+                                                <?php endif; ?>
+                                            </p> -->
+                                            <!-- <p class="works-link-text">ニーズ |
+                                                <?php
+                                                $work_needs = get_field('work_needs');
+                                                if ($work_needs) :
+                                                ?>
+                                                    <?php echo esc_html($work_needs); ?>
+                                                <?php else : ?>
+                                                <?php endif; ?>
+                                            </p> -->
+                        </div>
 
+                        <?php
+                                    $category_slug = get_field('work'); // ACFのフィールド名 'work'
+                                    if ($category_slug) :
+                        ?>
+                            <p><?php echo esc_html($category_slug); ?></p> <!-- スラッグを表示 -->
+                        <?php else : ?>
+                        <?php endif; ?>
 
-
-
-                                                <!-- タイトル：施工名 -->
-                                                <h3 class="works-link-text">
-                                                    <?php
-                                                    $work_name = get_field('work_name'); // カスタムフィールド 'work_name' 
-                                                    if ($work_name) : // あるか確認
-                                                    ?>
-                                                        <?php echo esc_html($work_name); ?><!-- テキスト表示 -->
-                                                    <?php else : ?>
-                                                        <!-- 文字列がない場合はデフォルトを表示 -->
-                                                    <?php endif; ?>
-                                                </h3>
-                                                <!-- カテゴリ -->
-                                                <p class="works-link-text">
-                                                    <?php
-                                                    $work_category = get_field('work_category'); // カスタムフィールド 'work_name' 
-                                                    if ($work_category) : // あるか確認
-                                                    ?>
-                                                        <?php echo esc_html($work_category); ?><!-- テキスト表示 -->
-                                                    <?php else : ?>
-                                                        <!-- 文字列がない場合はデフォルトを表示 -->
-                                                    <?php endif; ?>
-                                                </p>
-                                                <p class="works-link-text">
-                                                    <?php
-                                                    $work_category = get_field('work_category');
-                                                    if (is_array($work_category)) :
-                                                        echo esc_html(implode(', ', $work_category)); // 配列をカンマで区切って表示
-                                                    else :
-                                                        echo esc_html($work_category); // 文字列の場合
-                                                    endif;
-                                                    ?>
-                                                </p>
-                                                <p class="works-link-text">アピールポイント |
-                                                    <?php
-                                                    $work_strong = get_field('work_strong'); // カスタムフィールド 'work_name' 
-                                                    if ($work_strong) : // あるか確認
-                                                    ?>
-                                                        <?php echo esc_html($work_strong); ?><!-- テキスト表示 -->
-                                                    <?php else : ?>
-                                                        <!-- 文字列がない場合はデフォルトを表示 -->
-                                                    <?php endif; ?>
-                                                </p>
-                                                <p class="works-link-text" style="padding: 5px 10px; border-radius: 10px; border: 1px solid #232323; width: max-content;">#
-                                                    <?php
-                                                    $work_size = get_field('work_size'); // カスタムフィールド 'work_name' 
-                                                    if ($work_size) : // あるか確認
-                                                    ?>
-                                                        <?php echo esc_html($work_size); ?><!-- テキスト表示 -->
-                                                    <?php else : ?>
-                                                        <!-- 文字列がない場合はデフォルトを表示 -->
-                                                    <?php endif; ?>
-                                                </p>
-                                                <p class="works-link-text">ニーズ |
-                                                    <?php
-                                                    $work_needs = get_field('work_needs'); // カスタムフィールド 'work_name' 
-                                                    if ($work_needs) : // あるか確認
-                                                    ?>
-                                                        <?php echo esc_html($work_needs); ?><!-- テキスト表示 -->
-                                                    <?php else : ?>
-                                                        <!-- 文字列がない場合はデフォルトを表示 -->
-                                                    <?php endif; ?>
-                                                </p>
-                                            </div>
-
-                                            <?php
-                                            $category_slug = get_field('work'); // ACFのフィールド名 'work'
-                                            if ($category_slug) :
-                                            ?>
-                                                <p><?php echo esc_html($category_slug); ?></p> <!-- スラッグを表示 -->
-                                            <?php else : ?>
-                                                <p>デフォルトカテゴリ</p> <!-- デフォルトの文字列 -->
-                                            <?php endif; ?>
-
-                                        </a>
-                                    </li>
-                                <?php
+                        </a>
+                        </li>
+                    <?php
                                 }
                                 wp_reset_postdata();
-                                ?>
-                            </ul>
-                        </div>
-                    <?php
-                    }
                     ?>
+                    </ul>
                 </div>
+            <?php
+                    }
+            ?>
         </div>
     </div>
+</div>
 </div>
 
 
