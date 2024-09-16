6d7efaf8d67dff6795f596638a9aa21d40915b1f
lumijoe <spacedesign63@gmail.com>
2024-09-16 14:24:41 +0900

    test

--- page-work.php
+++ page-work.php
@@ -4,7 +4,6 @@ Template Name: 施工事例
 */
 get_header('contribution');
 ?>
-<!-- 施工事例一覧 -->
 <div class="page-inner full-width">
     <div class="page-main" id="pg-shopDetail">
         <div class="shopList-Container" style="margin-top: 0px!important; padding-top:90px!important; ">
@@ -16,68 +15,55 @@ get_header('contribution');
                 <div class="wrapper">
                     <?php
                     $args = array(
-                        'post_type' => 'works', //投稿タイプのスラッグを指定 
-                        'post_status' => 'publish', //公開済みの投稿を指定
-                        'posts_per_page' => -1, //投稿件数の指定(-1は全件取得)
+                        'post_type' => 'works',
+                        'post_status' => 'publish',
+                        'posts_per_page' => -1,
                     );
-                    // 配列にした記事を取得
-                    $the_query = new WP_Query($args); //記事を取得
-                    if ($the_query->have_posts()) {   //記事が取得できたか？
+
+                    $the_query = new WP_Query($args);
+                    if ($the_query->have_posts()) {
                     ?>
                         <div class="works-box" style="max-width: 300px;">
                             <ul class="works-lists">
                                 <?php
                                 while ($the_query->have_posts()) {
-                                    $the_query->the_post(); //ループのカウントアップ
+                                    $the_query->the_post();
                                 ?>
                                     <li class="works-list" style="list-style-type: none; max-width: 300px;">
-                                        <!-- パーマリンク -->
                                         <a class="works-link" href="<?php the_permalink(); ?>">
-                                            <!-- 画像：カスタムフィールドwork_imgから画像のURLを取得 -->
+                                            <!-- 画像 -->
                                             <div class="works-link-thumb">
                                                 <?php
                                                 $work_img = get_field('work_img');
-                                                // 画像があるか確認
                                                 if ($work_img) :
                                                 ?>
-                                                    <!-- 画像があればecho表示 -->
                                                     <img src="<?php echo esc_url($work_img); ?>" alt="" style="max-width: 300px; max-height: 220px; aspect-ratio: 300 / 220;">
                                                 <?php else : ?>
-                                                    <!-- 画像がなければデフォルトサムネを表示 -->
-                                                    <img src="<?php the_post_thumbnail_url(); ?>" alt="">
                                                 <?php endif; ?>
                                             </div>
-                                            <!-- タイトル：施工名 -->
+                                            <!-- 施工名 -->
                                             <h3 class="works-link-text" style="font-size: 16px; display: flex; padding: 5px 0px;">
                                                 <?php
-                                                $work_name = get_field('work_name'); // カスタムフィールド 'work_name' 
-                                                if ($work_name) : // あるか確認
+                                                $work_name = get_field('work_name');
+                                                if ($work_name) :
                                                 ?>
-                                                    <?php echo esc_html($work_name); ?><!-- テキスト表示 -->
+                                                    <?php echo esc_html($work_name); ?>
                                                 <?php else : ?>
-                                                    <!-- 文字列がない場合はデフォルトを表示 -->
                                                 <?php endif; ?>
                                             </h3>
-
+                                            <!-- タクソノミー -->
                                             <div class="works-link-body">
-                                                <!-- タクソノミーを取得する -->
-                                                <!-- タクソノミーが選択されていたら実行する -->
-                                                <!-- 複数のタクソノミーを配列で定義 -->
                                                 <div style="display: flex; flex-wrap: wrap; gap: 5px; max-width: 300px;">
                                                     <?php
                                                     $taxonomies = ['worktype', 'styletype', 'sizetype'];
-                                                    // 各タクソノミーごとのタームをループ処理
                                                     foreach ($taxonomies as $taxonomy) {
-                                                        // タクソノミーのタームを取得
                                                         $terms = get_the_terms($post->ID, $taxonomy);
                                                         if ($terms) {
                                                     ?>
                                                             <ul class="works-link-category" style="display: flex; flex-direction: row; gap:5px;">
-                                                                <!-- 取得できたタームの数だけループさせる -->
                                                                 <?php
                                                                 foreach ($terms as $term) {
                                                                 ?>
-                                                                    <!-- タームの名前を出力  -->
                                                                     <li style="padding: 2px 8px; border-radius: 10px; border: 1px solid #232323; width: max-content; font-size:12px;">＃<?php echo esc_html($term->name); ?>
                                                                     </li>
                                                                 <?php
@@ -88,8 +74,8 @@ get_header('contribution');
                                                         }
                                                     }
                                                     ?>
-                                                </div> <!--ここまでdiv ulループ-->
-                                            </div> <!--ここまでタクソノミーループdiv-->
+                                                </div>
+                                            </div>
 
                         </div>
 
